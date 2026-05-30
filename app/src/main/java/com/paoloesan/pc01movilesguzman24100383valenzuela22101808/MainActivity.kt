ea993921072359c54f39fd6952beeb6b6d0394e5
Brandon Valenzuela <22101808@ue.edu.pe>
2026-05-30 10:26:23 -0500

    Add Coil dependencies and configure DestinationsScreen
    - Add Coil Compose and OkHttp dependencies in `build.gradle.kts`.
    - Update `MainActivity` to display `DestinationsScreen` and remove placeholder greeting code.
    - Add `INTERNET` permission to `AndroidManifest.xml`.
    - Update project configuration to JDK 21 and include IDE-specific settings for VCS and deployment.

--- app/src/main/java/com/paoloesan/pc01movilesguzman24100383valenzuela22101808/MainActivity.kt
+++ app/src/main/java/com/paoloesan/pc01movilesguzman24100383valenzuela22101808/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.paoloesan.pc01movilesguzman24100383valenzuela22101808.presentation.destinations.DestinationsScreen
 import com.paoloesan.pc01movilesguzman24100383valenzuela22101808.ui.theme.PC01MOVILESGUZMAN24100383VALENZUELA22101808Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +18,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             PC01MOVILESGUZMAN24100383VALENZUELA22101808Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    DestinationsScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PC01MOVILESGUZMAN24100383VALENZUELA22101808Theme {
-        Greeting("Android")
     }
 }
